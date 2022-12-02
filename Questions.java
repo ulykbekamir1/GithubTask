197598b9c8478dd9769f981730074b5f4a524c7f
ulykbekamir1 <119683792+ulykbekamir1@users.noreply.github.com>
2022-12-02 20:51:46 +0600

    Create Questions.java

---
+++ Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: _______");
+        System.out.println("Surname: _______");
+        System.out.println("Id: _______");
+        System.out.println("Hobby: _______");
+        System.out.println("How much do you like ICT(from 1-10)): _______");
+        System.out.println("Rate this task from 1 to 10: _______");
+        System.out.println("Am I the best teacher?: _______");
+    }
+}
