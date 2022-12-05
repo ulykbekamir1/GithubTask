f8f6503d5e08fbe6be0a93d1087566435563380e
AlmazTalgatuly <119889469+AlmazTalgatuly@users.noreply.github.com>
2022-12-06 01:21:43 +0600

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("My favorite color?: _______");
+        System.out.println("Name: Almaz");
+        System.out.println("Surname: Talgatuly");
+        System.out.println("Id: 220103072");
+        System.out.println("Hobby: reading, sport");
+        System.out.println("How much do you like ICT(From 1-10): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("My favorite color: pink");
     }
 }
