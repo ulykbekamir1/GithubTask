654ed530b7e76c66d802363528666d12ec0680ab
jumysker <120036094+jumysker@users.noreply.github.com>
2022-12-08 12:50:28 +0600

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -7,6 +7,12 @@ public class Questions {
         System.out.println("How much do you like ICT(from 1-10)): _______");
         System.out.println("Rate this task from 1 to 10: _______");
         System.out.println("My favorite color?: _______");
-        
+        System.out.println("Name: Lashyn");
+        System.out.println("Surname: Maman");
+        System.out.println("Id: 220103371");
+        System.out.println("Hobby: watch k-dramas and read a book");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 9.999");
+        System.out.println("My favorite color?: black and blue");
     }
 }
