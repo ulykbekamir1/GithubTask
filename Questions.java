d69d33c2ff5ffc83644bf42d65200ae21f266000
220103199 <120212100+220103199@users.noreply.github.com>
2022-12-10 23:26:04 +0600

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -3,7 +3,7 @@ public class Questions {
         System.out.println("Name: Samal");
         System.out.println("Surname: Alibekova");
         System.out.println("Id: 220103199");
-        System.out.println("Hoby: Basketball
+        System.out.println("Hoby: Basketball");
         System.out.println("How much do you like ICT(from 1-10): 10);
         System.out.println("Rate this task from 1 to 10: 5");
         System.out.println("My favorite color?: Yellow);
