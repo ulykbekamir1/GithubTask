12248016dac101ddf59905bb8a8c84005d08c9b2
CharosYuldashbekova <120236321+CharosYuldashbekova@users.noreply.github.com>
2022-12-11 05:50:48 +0600

    name.java

---
+++ name.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Suleyman");
+        System.out.println("Surname: Demirel");
+        System.out.println("Id: 220104075");
+        System.out.println("Hobby: Football");
+        System.out.println("How much do you like ICT(from 1-10)): 5");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("My favorite color?: ");
+    }
+}
