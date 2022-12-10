47506f3fbe270e1fa79f3d149967f4c551ed1bf8
YeskendirDimash <120173573+YeskendirDimash@users.noreply.github.com>
2022-12-10 06:49:56 +0000

    ADD

--- Questions.java
+++ Questions.java
@@ -1,11 +1,12 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("My favorite color?: _______");
+        System.out.println("Name: Dimash");
+        System.out.println("Surname: Yeskendir");
+        System.out.println("Id: 220103327");
+        System.out.println("Hobby: Reading manga, swimming");
+        System.out.println("How much do you like ICT(from 1-10)): 7");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("My favorite color?: green");
     }
 }
+  
