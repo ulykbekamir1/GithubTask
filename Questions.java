ff55d87d9cbd78060f5267ca4d59988a122c50ba
Adilkhan Yerbolat <dilhanmq@gmail.com>
2022-12-10 23:11:34 +0600

    a

--- Questions.java
+++ Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("My favorite color?: _______");
+        System.out.println("Name: Yerbolat");
+        System.out.println("Surname: Adilkhan");
+        System.out.println("Id: 220103171");
+        System.out.println("Hobby: dissecting some videotapes, returning girls");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 1");
+        System.out.println("My favorite color?: You`ve said that is black");
     }
 }
