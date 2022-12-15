55d2fff5b261955750694cd6151b2bbcd103aa9c
ZhanatKu <116269986+ZhanatKu@users.noreply.github.com>
2022-12-16 02:00:15 +0600

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: Zhanat);
-        System.out.println("Surname: Kuanyshov);
+        System.out.println("Name: Zhanat");
+        System.out.println("Surname: Kuanyshov");
         System.out.println("Id: 220103283");
-        System.out.println("Hobby: Watch anime);
+        System.out.println("Hobby: Watch anime");
         System.out.println("How much do you like ICT(from 1-10)): 10");
         System.out.println("Rate this task from 1 to 10: 10");
-        System.out.println("My favorite color?: Gray);
+        System.out.println("My favorite color?: Gray");
     }
 }
